ece8d1a81fddb548383dd5d49e685a5465bad256
Nadir0glu <nadiroglunail@gmail.com>
2024-03-01 18:50:25 -0500

    nail

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,31 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
+import { useParams } from "react-router-dom";
 
 function Home() {
+
+  const [page, setPage] = useState([])
+  console.log(page);
+
+  useEffect(()=> {
+    fetch(`http://localhost:4000/movies`)
+    .then((r)=> r.json())
+    .then((d) => setPage(d))
+  },[])
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+       <NavBar/>
       </header>
       <main>
-        {/* Info goes here! */}
+        
+        <h1>Home Page</h1>
+        {page.map((m)=> {
+          return <MovieCard id={m.id} movie={m} key={m.id} title={m.title}/>
+        })}
+       
       </main>
     </>
   );
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,34 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+
 
 function Movie() {
+
+  const [movie, setMovie] = useState([])
+
+  const params = useParams()
+  const movieId = params.id
+  console.log(params);
+  console.log(movie);
+
+
+ useEffect(()=> {
+  fetch(`http://localhost:4000/movies`)
+  .then((r)=> r.json())
+  .then((d) => {
+    setMovie(d)
+  })
+ },[movieId])
+
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
-    </>
+    <div>
+      <h1>Title: {movie.title}</h1>
+      <p>Time: {movie.time}</p>
+      
+    </div>
   );
 };
+;
 
 export default Movie;
